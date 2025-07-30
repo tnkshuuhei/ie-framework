6a53d52ae0abb438d3ce7117df4db64a54554c89
shuhei tanaka <shuhei46491@gmail.com>
2025-07-30 15:46:15 +0000

    feat: initialize ISplitMain.sol

---
+++ contracts/Interfaces/ISplitMain.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.29;
+
+// Ref: https://github.com/0xSplits/splits-contracts/blob/main/contracts/interfaces/ISplitMain.sol
+interface ISplitMain {
+    function createSplit(
+        address[] calldata accounts,
+        uint32[] calldata percentAllocations,
+        uint32 distributorFee,
+        address controller
+    )
+        external
+        returns (address);
+
+    function predictImmutableSplitAddress(
+        address[] calldata accounts,
+        uint32[] calldata percentAllocations,
+        uint32 distributorFee
+    )
+        external
+        view
+        returns (address);
+
+    function updateSplit(
+        address split,
+        address[] calldata accounts,
+        uint32[] calldata percentAllocations,
+        uint32 distributorFee
+    )
+        external;
+
+    function distributeETH(
+        address split,
+        address[] calldata accounts,
+        uint32[] calldata percentAllocations,
+        uint32 distributorFee,
+        address distributorAddress
+    )
+        external;
+}
