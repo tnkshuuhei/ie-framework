4ece0ad6ab77fb1af8d203eb30076c7745c22579
shuhei tanaka <shuhei46491@gmail.com>
2025-07-31 22:59:51 +0000

    feat: enhance ImpactEvaluator with custom error handling and validation checks for pool creation

--- contracts/ImpactEvaluator.sol
+++ contracts/ImpactEvaluator.sol
@@ -8,6 +8,14 @@ import { IHatsHatCreatorModule } from "./Hats/IHatCreatorModule.sol";
 import { IHatsTimeControlModule } from "./Hats/ITimeControlModule.sol";
 
 contract IE {
+    // Custom errors
+    error ZeroAddress();
+    error InvalidArrayLength();
+    error InvalidAllocationPercentage();
+    error PoolDoesNotExist();
+    error EmptyArray();
+    error InvalidMetadata();
+
     struct PoolConfig {
         address admin;
         address[] recipients;
@@ -59,6 +67,14 @@ contract IE {
         address _hatCreatorModuleImpl,
         address _timeControlModuleImpl
     ) {
+        require(_owner != address(0), ZeroAddress());
+        require(_hats != address(0), ZeroAddress());
+        require(_splits != address(0), ZeroAddress());
+        require(_hatsModuleFactory != IHatsModuleFactory(address(0)), ZeroAddress());
+        require(_hatCreatorModuleImpl != address(0), ZeroAddress());
+        require(_timeControlModuleImpl != address(0), ZeroAddress());
+        require(bytes(_topHatMetadata).length > 0, InvalidMetadata());
+
         hats = IHats(_hats);
         splits = ISplitMain(_splits);
 
@@ -70,6 +86,8 @@ contract IE {
 
     // TODO: split into multiple functions
     function createPool(bytes memory _data /*, Module[] memory _modules*/ ) external {
+        require(_data.length > 0, InvalidMetadata());
+
         (
             address admin,
             address[] memory recipients,
@@ -80,6 +98,45 @@ contract IE {
             string memory recipientHatMetadata,
             address[] memory evaluators
         ) = abi.decode(_data, (address, address[], uint32[], string, string, string, string, address[]));
+
+        // Validate admin address
+        require(admin != address(0), ZeroAddress());
+
+        // Validate recipients array
+        require(recipients.length > 0, EmptyArray());
+        require(recipients.length == initialAllocations.length, InvalidArrayLength());
+
+        // Validate evaluators array
+        require(evaluators.length > 0, EmptyArray());
+
+        // Validate metadata strings
+        require(bytes(managerHatMetadata).length > 0, InvalidMetadata());
+        require(bytes(evaluatorHatMetadata).length > 0, InvalidMetadata());
+        require(bytes(recipientHatMetadata).length > 0, InvalidMetadata());
+
+        // Validate allocation percentages
+        uint32 totalAllocation = 0;
+        for (uint256 i = 0; i < initialAllocations.length; i++) {
+            totalAllocation += initialAllocations[i];
+        }
+        require(totalAllocation == 10_000, InvalidAllocationPercentage());
+
+        // Validate no duplicate recipients
+        for (uint256 i = 0; i < recipients.length; i++) {
+            require(recipients[i] != address(0), ZeroAddress());
+            for (uint256 j = i + 1; j < recipients.length; j++) {
+                require(recipients[i] != recipients[j], ZeroAddress());
+            }
+        }
+
+        // Validate no duplicate evaluators
+        for (uint256 i = 0; i < evaluators.length; i++) {
+            require(evaluators[i] != address(0), ZeroAddress());
+            for (uint256 j = i + 1; j < evaluators.length; j++) {
+                require(evaluators[i] != evaluators[j], ZeroAddress());
+            }
+        }
+
         PoolConfig memory pool = PoolConfig({
             admin: admin,
             recipients: recipients,
@@ -159,8 +216,25 @@ contract IE {
             address recipient = recipients[i];
             IHatsTimeControlModule(timeControlModule).mintHat(recipientHatId, recipient, block.timestamp);
         }
+
+        emit PoolCreated(
+            poolCount, managerHatId, splitsContract, evaluatorHatId, recipientHatId, evaluators, recipients
+        );
     }
 
-    // function to evaluate
-    // function to update splits contract
+    function evaluate(uint256 _poolId) external {
+        require(_poolId > 0 && _poolId <= poolCount, PoolDoesNotExist());
+
+        address splitsContract = splitsContracts[_poolId];
+        require(splitsContract != address(0), ZeroAddress());
+
+        address[] memory accounts = pools[_poolId].recipients;
+        require(accounts.length > 0, EmptyArray());
+
+        uint32[] memory percentAllocations;
+
+        // TODO: need to calculate percentAllocations based on time weighted allocation with time control module
+
+        splits.updateSplit(splitsContract, accounts, percentAllocations, 0);
+    }
 }
