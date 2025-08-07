8f7a1f354d22cc620e5670102148156372c78e12
shuhei tanaka <shuhei46491@gmail.com>
2025-08-07 16:27:05 +0000

    feat: add ProtocolGuild contract and corresponding tests for IE creation and evaluation functionality

---
+++ contracts/IEstrategies/ProtocolGuild.sol
@@ -0,0 +1,317 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
+
+import { IEAS, Attestation, AttestationRequest, AttestationRequestData } from "eas-contracts/IEAS.sol";
+import { BaseIEStrategy } from "./BaseIEStrategy.sol";
+import { ISplitMain } from "../interfaces/ISplitMain.sol";
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
+import { AttesterResolver } from "../AttesterResolver.sol";
+import { IScaffoldIE } from "../interfaces/IScaffoldIE.sol";
+import { IStrategy } from "../interfaces/IStrategy.sol";
+
+contract ProtocolGuild is BaseIEStrategy, AccessControl, Pausable {
+    // State variables
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant EVALUATOR_ROLE = keccak256("EVALUATOR_ROLE");
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+
+    string public schema;
+    bytes32 public schemaUID;
+    address public splitsContract;
+
+    mapping(address => uint256) public startDate;
+    mapping(address => bool) public isActive;
+    mapping(address => WorkType) public workType;
+
+    error RecipientsAndWorkTypesLengthMismatch();
+    error InitialAllocationsLengthMismatch();
+    error InvalidAllocation();
+
+    enum WorkType {
+        FULL,
+        PARTIAL
+    }
+
+    mapping(WorkType => uint256) public workTypeToMultiplier;
+
+    IEAS public eas;
+
+    // Events
+    event AttestationCreated(bytes32 attestationUID);
+    event Evaluated(address[] recipients, uint32[] allocations);
+
+    // Errors
+    error InvalidEvaluator(address _caller);
+    error InvalidManager(address _caller);
+
+    /// @param _poolId The pool ID
+    /// @param _initializeData The initialization data
+    /// @param _scaffoldIE The scaffold IE address
+    function initialize(uint256 _poolId, bytes memory _initializeData, address _scaffoldIE) external override {
+        // Check that the caller is the ScaffoldIE contract
+        require(msg.sender == _scaffoldIE, OnlyScaffoldIE(msg.sender));
+
+        scaffoldIE = IScaffoldIE(_scaffoldIE);
+
+        __BaseStrategyInit(_poolId, _initializeData);
+    }
+
+    /// @param _initializeData The initialization data
+    function _initialize(bytes memory _initializeData) internal override {
+        name = "ProtocolGuild";
+
+        (address _eas, bytes32 _schemaUID, address _admin) = abi.decode(_initializeData, (address, bytes32, address));
+        eas = IEAS(_eas);
+        schemaUID = _schemaUID;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+        _grantRole(PAUSER_ROLE, _admin);
+    }
+
+    // Public/External functions
+
+    /// @return The splits contract address
+    function getAddress() external view returns (address) {
+        return splitsContract;
+    }
+
+    /// @param _data The data for creating the IE
+    function createIE(bytes memory _data) external override onlyScaffoldIE onlyInitialized {
+        _createIE(_data);
+    }
+
+    /// @param _data The evaluation data
+    /// @param _caller The caller address
+    function evaluate(
+        bytes memory _data,
+        address _caller
+    )
+        external
+        override
+        onlyEvaluator(_caller)
+        onlyScaffoldIE
+        onlyInitialized
+    {
+        _beforeEvaluation(_data);
+        _evaluate(_data);
+    }
+
+    /// @param _data The data for registering the recipients
+    /// @param _caller The caller address
+    function registerRecipients(
+        bytes memory _data,
+        address _caller
+    )
+        external
+        override
+        onlyManager(_caller)
+        onlyScaffoldIE
+        onlyInitialized
+    {
+        _registerRecipients(_data);
+    }
+
+    /// @param _data The data for registering the recipients
+    function _registerRecipients(bytes memory _data) internal override {
+        (address[] memory _recipients, WorkType[] memory _workTypes) = abi.decode(_data, (address[], WorkType[]));
+        recipientsData = abi.encode(_recipients, _workTypes);
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            workType[_recipients[i]] = _workTypes[i];
+        }
+    }
+
+    /// @param _data The data for updating the recipients
+    /// @param _caller The caller address
+    function updateRecipients(
+        bytes memory _data,
+        address _caller
+    )
+        external
+        override
+        onlyManager(_caller)
+        onlyScaffoldIE
+        onlyInitialized
+    {
+        _updateRecipients(_data);
+    }
+
+    /// @param _data The data for updating the recipients
+    function _updateRecipients(bytes memory _data) internal override {
+        (address[] memory _recipients, WorkType[] memory _workTypes) = abi.decode(_data, (address[], WorkType[]));
+        recipientsData = abi.encode(_recipients, _workTypes);
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            workType[_recipients[i]] = _workTypes[i];
+        }
+    }
+
+    // Internal functions
+
+    /// @notice Create an IE
+    /// @param _data The data to create the IE
+    /// @dev The data is a string, address[], uint32[]
+    /// @dev address[]: recipients
+    /// @dev uint32[]: initial allocations
+    function _createIE(bytes memory _data) internal override {
+        (address[] memory _recipients, WorkType[] memory _workTypes, uint32[] memory _initialAllocations) =
+            abi.decode(_data, (address[], WorkType[], uint32[]));
+
+        require(_recipients.length == _workTypes.length, RecipientsAndWorkTypesLengthMismatch());
+        require(_recipients.length == _initialAllocations.length, InitialAllocationsLengthMismatch());
+
+        recipientsData = abi.encode(_recipients, _workTypes);
+
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            workType[_recipients[i]] = _workTypes[i];
+            startDate[_recipients[i]] = block.timestamp;
+        }
+
+        splitsContract =
+            ISplitMain(scaffoldIE.getSplits()).createSplit(_recipients, _initialAllocations, 0, address(this));
+    }
+
+    function _beforeEvaluation(bytes memory _data) internal override { }
+
+    function _evaluate(bytes memory _data) internal override {
+        (address[] memory recipients, WorkType[] memory workTypes) = abi.decode(recipientsData, (address[], WorkType[]));
+        uint32[] memory allocations = _calculateAllocations(recipients, workTypes);
+        _processEvaluation(_data, recipients, allocations);
+    }
+
+    function _calculateAllocations(
+        address[] memory recipients,
+        WorkType[] memory workTypes
+    )
+        internal
+        view
+        returns (uint32[] memory)
+    {
+        uint32[] memory allocations = new uint32[](recipients.length);
+        uint256 timestamp = block.timestamp;
+        uint256 totalAllocations = 0;
+
+        // First pass: calculate total allocations
+        for (uint256 i = 0; i < recipients.length; i++) {
+            uint256 timeDiff = timestamp - startDate[recipients[i]];
+            // if (timeDiff > 365 days) timeDiff = 365 days;
+            uint256 sqrtTimeDiff = Math.sqrt(timeDiff);
+            uint256 weighted = workTypes[i] == WorkType.FULL ? sqrtTimeDiff * 10 : sqrtTimeDiff * 5;
+            totalAllocations += weighted;
+        }
+
+        require(totalAllocations > 0, InvalidAllocation());
+
+        // Second pass: calculate scaled allocations
+        uint256 totalScaled = 0;
+        for (uint256 i = 0; i < recipients.length - 1; i++) {
+            uint256 timeDiff = timestamp - startDate[recipients[i]];
+            // if (timeDiff > 365 days) timeDiff = 365 days;
+            uint256 sqrtTimeDiff = Math.sqrt(timeDiff);
+            uint256 weighted = workTypes[i] == WorkType.FULL ? sqrtTimeDiff * 10 : sqrtTimeDiff * 5;
+
+            uint256 scaledAllocation = (weighted * 1_000_000) / totalAllocations;
+            require(scaledAllocation <= type(uint32).max, InvalidAllocation());
+            allocations[i] = uint32(scaledAllocation);
+            totalScaled += scaledAllocation;
+        }
+
+        // Last element gets remaining allocation
+        uint256 remainingAllocation = 1_000_000 - totalScaled;
+        require(remainingAllocation <= type(uint32).max, InvalidAllocation());
+        allocations[recipients.length - 1] = uint32(remainingAllocation);
+
+        return allocations;
+    }
+
+    function _processEvaluation(
+        bytes memory _data,
+        address[] memory recipients,
+        uint32[] memory allocations
+    )
+        internal
+    {
+        _sortAddressesAndAllocations(recipients, allocations);
+
+        (string memory _dataset, address contractAddress, uint256 chainId, address evaluator) =
+            abi.decode(_data, (string, address, uint256, address));
+
+        bytes memory attestationData =
+            abi.encode(_dataset, recipients, allocations, contractAddress, chainId, evaluator);
+
+        AttestationRequestData memory attestationRequestData = AttestationRequestData({
+            recipient: address(0),
+            expirationTime: 0,
+            revocable: true,
+            refUID: bytes32(0),
+            data: attestationData,
+            value: 0
+        });
+        AttestationRequest memory request = AttestationRequest({ schema: schemaUID, data: attestationRequestData });
+
+        eas.attest(request);
+
+        ISplitMain(scaffoldIE.getSplits()).updateSplit(splitsContract, recipients, allocations, 0);
+        emit Evaluated(recipients, allocations);
+    }
+
+    function _beforeCreateIE(bytes memory _data) internal override {
+        revert NotImplemented();
+    }
+
+    function _afterEvaluation(bytes memory _data) internal override {
+        revert NotImplemented();
+    }
+
+    function getStartDate(address _recipient) external view returns (uint256) {
+        return startDate[_recipient];
+    }
+
+    function getWorkType(address _recipient) external view returns (WorkType) {
+        return workType[_recipient];
+    }
+
+    function getWorkTime(address _recipient) external view returns (uint256) {
+        return block.timestamp - startDate[_recipient];
+    }
+
+    function setWorkTypeToMultiplier(WorkType _workType, uint256 _multiplier) external onlyAdmin(msg.sender) {
+        workTypeToMultiplier[_workType] = _multiplier;
+    }
+
+    function getRecipients() external view returns (bytes memory) {
+        return recipientsData;
+    }
+
+    function addEvaluator(address _evaluator, address _caller) external onlyAdmin(_caller) onlyInitialized {
+        _grantRole(EVALUATOR_ROLE, _evaluator);
+    }
+
+    function removeEvaluator(address _evaluator, address _caller) external onlyAdmin(_caller) onlyInitialized {
+        _revokeRole(EVALUATOR_ROLE, _evaluator);
+    }
+
+    function addManager(address _manager, address _caller) external onlyAdmin(_caller) onlyInitialized {
+        _grantRole(MANAGER_ROLE, _manager);
+    }
+
+    function removeManager(address _manager, address _caller) external onlyAdmin(_caller) onlyInitialized {
+        _revokeRole(MANAGER_ROLE, _manager);
+    }
+
+    // Modifiers
+    modifier onlyEvaluator(address _caller) {
+        require(hasRole(EVALUATOR_ROLE, _caller), InvalidEvaluator(_caller));
+        _;
+    }
+
+    modifier onlyManager(address _caller) {
+        require(hasRole(MANAGER_ROLE, _caller), InvalidManager(_caller));
+        _;
+    }
+
+    modifier onlyAdmin(address _caller) {
+        require(hasRole(DEFAULT_ADMIN_ROLE, _caller), "Not admin");
+        _;
+    }
+}
---
+++ test/ProtocolGuild.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+
+import { Base } from "./Base.t.sol";
+import { ScaffoldIE } from "../contracts/ScaffoldIE.sol";
+import { ProtocolGuild } from "../contracts/IEstrategies/ProtocolGuild.sol";
+import { IStrategy } from "../contracts/interfaces/IStrategy.sol";
+
+contract ProtocolGuildTest is Base {
+    bytes32 public schemaUID;
+    ProtocolGuild public protocolGuild;
+
+    function setUp() public {
+        _configureChain();
+        schemaUID = 0x78add97290831dd54d5e4599a0a1dc1ada8278264c93c801d72adddea395e26f;
+        scaffoldIE = new ScaffoldIE(admin, address(splits));
+        protocolGuild = new ProtocolGuild(); // implementation
+
+        vm.startPrank(admin);
+        scaffoldIE.setCloneableStrategy(address(protocolGuild), true);
+        vm.stopPrank();
+    }
+
+    function testDeployments() public view {
+        assertNotEq(address(scaffoldIE), address(0));
+        assertEq(scaffoldIE.getSplits(), address(splits));
+        assertNotEq(address(protocolGuild), address(0));
+    }
+
+    function testCreateIE() public {
+        address[] memory _recipients = new address[](5);
+        _recipients[0] = recipient1;
+        _recipients[1] = recipient2;
+        _recipients[2] = recipient3;
+        _recipients[3] = recipient4;
+        _recipients[4] = address(5);
+
+        ProtocolGuild.WorkType[] memory workTypes = new ProtocolGuild.WorkType[](5);
+        workTypes[0] = ProtocolGuild.WorkType.FULL;
+        workTypes[1] = ProtocolGuild.WorkType.PARTIAL;
+        workTypes[2] = ProtocolGuild.WorkType.PARTIAL;
+        workTypes[3] = ProtocolGuild.WorkType.FULL;
+        workTypes[4] = ProtocolGuild.WorkType.PARTIAL;
+
+        uint32[] memory _initialAllocations = new uint32[](5);
+        _initialAllocations[0] = 2e5;
+        _initialAllocations[1] = 2e5;
+        _initialAllocations[2] = 2e5;
+        _initialAllocations[3] = 2e5;
+        _initialAllocations[4] = 2e5;
+
+        bytes memory data = abi.encode(_recipients, workTypes, _initialAllocations);
+
+        bytes memory initializeData = abi.encode(address(eas), schemaUID, admin);
+
+        // Execute test
+        vm.startPrank(admin);
+        scaffoldIE.createIE(data, initializeData, address(protocolGuild));
+
+        // Verify results
+        assertEq(scaffoldIE.getPoolCount(), 1);
+        // Get the actual strategy address that was created
+        address actualStrategy = scaffoldIE.getStrategy(0);
+        assertNotEq(actualStrategy, address(0));
+        vm.stopPrank();
+    }
+
+    function testEvaluateIE() public {
+        testCreateIE();
+
+        bytes memory evaluationData = abi.encode("data", address(protocolGuild), block.chainid, evaluator);
+
+        vm.startPrank(admin);
+        scaffoldIE.addEvaluator(0, evaluator, admin);
+        vm.stopPrank();
+        // warp 3months
+        vm.warp(block.timestamp + 13 * 30 days);
+
+        vm.startPrank(evaluator);
+        scaffoldIE.evaluate(0, evaluationData, evaluator);
+        vm.stopPrank();
+    }
+}
